b57ee9ef91abd980f12e263e3fb0423ee8abf93a
aryy <aaryan.p114@gmail.com>
2021-03-04 21:58:39 +0530

    increased speed

--- LoginFaceManual.py
+++ LoginFaceManual.py
@@ -1,16 +1,15 @@
 from tkinter import Tk, PhotoImage, Button, Label, StringVar, Entry
 from threading import Thread as Process
 import cv2
-from numpy import argmin
-from face_recognition import face_locations, face_encodings, compare_faces, face_distance
+import time
+from face_recognition import face_encodings, compare_faces, load_image_file
 import os
 from PIL import ImageTk, Image
-
-
 def pascheck(idt, past):
     if idt == "StartCode@@@" and past == "12121@!#":
         root.after(1000, root.destroy)
         print("yo")
+        #################################### Your Code after login ###################################
         sroot = Tk()
         sroot.title("login successfull")
         sroot.geometry("300x200")
@@ -20,16 +19,13 @@ def pascheck(idt, past):
         idlab = Label(sroot, text="Hello User", fg="white", bg="black", font=("Segoe UI", 18))
         idlab.place(relx=0.2, rely=0.3)
         sroot.mainloop()
+        ##############################################################################################
     else:
         print("Unauthorized Access")
-
-
 def paschecklog(idt, past):
     global auth
     if idt == "ary" and past == "1234":
         auth = True
-
-
 def loginGui():
     global idv
     global pas
@@ -56,50 +52,29 @@ def loginGui():
     signin.image = bg
     signin.place(rely=0.7, relx=0.36)
     root.mainloop()
-
-
 def facelog():
     global auth
     path = "IMGD"
-    images = []
-    classNames = []
-    myList = os.listdir(path)
-    print(myList)
-    for cl in myList:
-        curImg = cv2.imread(f'{path}/{cl}')
-        images.append(curImg)
-        classNames.append(os.path.splitext(cl)[0])
-    print(classNames)
-
-    def findEncodings(images):
-        encodeList = []
-        for img in images:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            encode = face_encodings(img)[0]
-            encodeList.append(encode)
-        return encodeList
-
-    encodeListKnown = findEncodings(images)
+    known_image = load_image_file(path+"/model.png")
+    encoding = face_encodings(known_image)[0]
     cap = cv2.VideoCapture(0)
     while not auth:
         success, img = cap.read()
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-        facesCurFrame = face_locations(imgS)
-        encodesCurFrame = face_encodings(imgS, facesCurFrame)
-        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-            matches = compare_faces(encodeListKnown, encodeFace, tolerance=0.9)
-            faceDis = face_distance(encodeListKnown, encodeFace)
-            matchIndex = argmin(faceDis)
-            if matches[matchIndex]:
-                auth = True
+        try:
+            unknown_image = face_encodings(imgS)[0]
+            results = compare_faces([encoding], unknown_image, tolerance=0.5)
+        except:
+            results = [False]
+        if results[0]:
+            break
         cv2.waitKey(1)
     cap.release()
     cv2.destroyAllWindows()
     pascheck('StartCode@@@', '12121@!#')
-
-
 if __name__ == "__main__":
+    ad = time.time()
     if not os.path.exists('IMGD'):
         os.makedirs('IMGD')
     if not os.listdir('./IMGD'):
@@ -128,3 +103,4 @@ if __name__ == "__main__":
     a = Process(target=facelog, args="")
     a.start()
     loginGui()
+    print(round(time.time()-ad, 2),"s Taken to login")
